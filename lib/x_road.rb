94dd20d717e7bbe214fdc56075a697e445e370be
Ilia Aphtsiauri <ilia.aptsiauri@veriff.me>
2017-09-28 18:47:11 +0300

    implements xroad_v6 integration

--- lib/x_road.rb
+++ lib/x_road.rb
@@ -2,10 +2,7 @@ require 'require_all'
 require 'x_road/version'
 require 'uuidtools'
 require 'savon'
-require 'x_road/active_x_road'
-require 'x_road/active_x_road2'
 require 'x_road/active_x_road3'
-require 'x_road/kpr/laekumised_maksuametist.rb'
 
 module XRoad
   class << self
@@ -21,16 +18,37 @@ module XRoad
   end
 
   class Configuration
+    Client = Struct.new(:member_class, :member_code, :subsystem_code)
+    Service = Struct.new(:member_class, :member_code, :subsystem_code, :service_code, :version)
+
+    attr_accessor :instance
+    attr_accessor :protocol_version
     attr_accessor :host
     attr_accessor :client_cert
     attr_accessor :client_key
     attr_accessor :log_level
-    attr_accessor :consumer
     attr_accessor :ssl_verify
+
+    attr_reader :service
+    attr_reader :client
 
     def initialize
       @log_level = :info
       @ssl_verify = :none
+    end
+
+    def client=(opts)
+      @client = Client.new(opts[:member_class],
+                           opts[:member_code],
+                           opts[:subsystem_code])
+    end
+
+    def service=(opts)
+      @service = Service.new(opts[:member_class],
+                             opts[:member_code],
+                             opts[:subsystem_code],
+                             opts[:service_code],
+                             opts[:version])
     end
   end
 end
--- lib/x_road/active_x_road3.rb
+++ lib/x_road/active_x_road3.rb
@@ -1,14 +1,34 @@
 module XRoad
   class ActiveXRoad3
-    def self.create_header(producer, user_id, service)
+    def self.create_header(user_id)
+      client = config.client
+      service = config.service
       id = UUIDTools::UUID.random_create.to_s
       id.delete! '-'
       {
-        "xroad:id" => id,
-        "xroad:service" => service,
-        "xroad:consumer" => XRoad.configuration.consumer,
-        "xroad:producer" => producer,
-        "xroad:userId" => user_id
+        'xrd:client' => {
+          '@id:objectType': 'SUBSYSTEM',
+          content!: {
+            'id:xRoadInstance': config.instance,
+            'id:memberClass': client.member_class,
+            'id:memberCode': client.member_code,
+            'id:subsystemCode': client.subsystem_code
+          }
+        },
+        "xrd:service" => {
+          '@id:objectType': 'SERVICE',
+          content!: {
+            'id:xRoadInstance': config.instance,
+            'id:memberClass': service.member_class,
+            'id:memberCode': service.member_code,
+            'id:subsystemCode': service.subsystem_code,
+            'id:serviceCode': service.service_code,
+            'id:serviceVersion': service.version
+          }
+        },
+        'xrd:id': id,
+        'xrd:userId': user_id,
+        'xrd:protocolVersion': config.protocol_version
       }
     end
 
@@ -23,23 +43,28 @@ module XRoad
     end
 
     def self.create_client(ns)
-      config = XRoad.configuration
+      conf = config
       Savon.client do
-        endpoint config.host
-        ssl_cert_file config.client_cert
-        ssl_cert_key_file config.client_key
+        endpoint conf.host
+        ssl_cert_file conf.client_cert
+        ssl_cert_key_file conf.client_key
         pretty_print_xml true
-        ssl_verify_mode config.ssl_verify
+        ssl_verify_mode conf.ssl_verify
         log true
-        log_level config.log_level
-				convert_attributes_to proc { [] }
-        namespace_identifier "prdcr"
+        log_level conf.log_level
+        convert_attributes_to proc { [] }
+        namespace_identifier "prod"
         namespace ns
         namespaces(
-          "xmlns:xroad" => "http://x-road.ee/xsd/x-road.xsd",
-          "xmlns:prdcr" => ns
+          "xmlns:xrd" => "http://x-road.eu/xsd/xroad.xsd",
+          "xmlns:id" => "http://x-road.eu/xsd/identifiers",
+          "xmlns:prod" => ns
         )
       end
+    end
+
+    def self.config
+      XRoad.configuration
     end
   end
 end
